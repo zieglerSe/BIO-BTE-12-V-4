da22bd360e73fba9b65b5c0b4d5bf6f35c31939d
kMutagene <schneider.kev@outlook.de>
2021-04-16 17:38:40 +0000

    Deploying to gh-pages from  @ 0ceb09e1f1a21f36b78d347bc71195f35d462d9c 🚀

--- tasks/english/installation-instructions.fsx
+++ tasks/english/installation-instructions.fsx
@@ -1,7 +1,9 @@
 (**
-// can't yet format YamlFrontmatter (["title: Installation instructions"; "category: Tasks(english)"; "categoryindex: 3"; "index: 1"], Some { StartLine = 2 StartColumn = 0 EndLine = 6 EndColumn = 8 }) to pynb markdown
+// can't yet format YamlFrontmatter (["title: Getting Started"; "category: Tasks(english)"; "categoryindex: 3"; "index: 1"], Some { StartLine = 2 StartColumn = 0 EndLine = 6 EndColumn = 8 }) to pynb markdown
 
-# Installation
+# Getting Started
+
+## Installation instructions
 
 * Download the latest stable build for [Visual Studio Code](https://code.visualstudio.com/) and install it.
 * Download the recommended [.NET SDK](https://dotnet.microsoft.com/download) and install it.
@@ -9,30 +11,74 @@
     * .NET Interactive Notebooks
     * Ionide-fsharp
     
-    ![](https://raw.githubusercontent.com/CSBiology/BIO-BTE-12-V-4/main/docs/img/CodeExtensions.png)
+    ![](/BIO-BTE-12-V-4/img/CodeExtensions.png)
+
+
+## Working on Exercises
+
+There are three different possibilities to work on the exercises:
+
+* Locally as notebook in Visual Studio Code
+* Locally as fsx in Visual Studio Code
+* Online as notebook on Binder
+
+### Locally as notebook in Visual Studio Code
 
 * Download the current notebook from the page linked on the left.
 
-    ![](https://raw.githubusercontent.com/CSBiology/BIO-BTE-12-V-4/main/docs/img/DownloadNotebook.png)
+    ![](/BIO-BTE-12-V-4/img/DownloadNotebook.png)
 
-* In Visual Studio Code press `Strg + Shift + P` or `F1` and klick on `.NET Interactive Open notebook`.
+* In Visual Studio Code press `Ctrl + Shift + P` or `F1` and klick on `.NET Interactive Open notebook`.
 
-    ![](https://raw.githubusercontent.com/CSBiology/BIO-BTE-12-V-4/main/docs/img/OpenNotebook.png)
+    ![](/BIO-BTE-12-V-4/img/OpenNotebook.png)
 
 * Navigate to the location of your notebook and open it.
 * Notebooks contain Text- and Codeblocks:
     * Adding a new Text- or Codeblock can be done by hovering at the upper or lower border of an existing block:
 
-    ![](https://raw.githubusercontent.com/CSBiology/BIO-BTE-12-V-4/main/docs/img/AddingBlock.png)
+    ![](/BIO-BTE-12-V-4/img/AddingBlock.png)
 
     * Working with Textblocks:
         You can edit a Textblock by doubleklicking on it. Inside a Textblock you can write plain text or style it with [Markdown](https://en.wikipedia.org/wiki/Markdown).
         Once you are finished you can press the `Esc` button.
     * Working with Codeblocks:
-        You can start editing any Codeblock by clicking in it. In there you can start writing your own code or edit existing code. Once you are done you can execute the Codeblock by pressing `Strg + Alt + Enter`.
+        You can start editing any Codeblock by clicking in it. In there you can start writing your own code or edit existing code. Once you are done you can execute the Codeblock by pressing `Ctrl + Alt + Return`.
         If you want to execute all codeblocks at once, you can press on the two arrows in the upper right corner of the notebook:
 
-    ![](https://raw.githubusercontent.com/CSBiology/BIO-BTE-12-V-4/main/docs/img/ExecuteAll.png)
+    ![](/BIO-BTE-12-V-4/img/ExecuteAll.png)
+
+### Locally as fsx in Visual Studio Code
+
+* Download the current fsx from the page linked on the left.
+
+    ![](/BIO-BTE-12-V-4/img/DownloadScript.png)
+
+* In Visual Studio Code press `Ctrl + O`, navigate to the location of your fsx and open it.
+* You can write code anywhere in the fsx file, similar to a normal text file, and execute it by pressing `Alt + Return`
+    * Pressing `Alt + Return` in a line will execute the current line
+    * Marking a section and pressing `Alt + Return` will execute the marked section
+* You can comment your code by prefixing your comment with "//" (e.g.: // My Comment)
+
+### Online as notebook on Binder
+
+* Open the current notebook from the page linked on the left.
+
+    ![](/BIO-BTE-12-V-4/img/RunBinder.png)
+
+* Notebooks contain Text- and Codeblocks:
+    * Adding a new Text- or Codeblock can be done by clicking `Insert` at the top of the page and choosing `Code` or `Markdown` as property:
+
+    ![](/BIO-BTE-12-V-4/img/BinderAddingBlock.png)
+
+    * Working with Textblocks:
+        You can edit a Textblock by doubleklicking on it. Inside a Textblock you can write plain text or style it with [Markdown](https://en.wikipedia.org/wiki/Markdown).
+        Once you are finished you can press `Ctrl + Return`.
+    * Working with Codeblocks:
+        You can start editing any Codeblock by clicking in it. In there you can start writing your own code or edit existing code. Once you are done you can execute the Codeblock by pressing `Ctrl + Return`.
+        If you want to execute all codeblocks at once, you can klick on the two arrows at the top of the notebook:
+
+    ![](/BIO-BTE-12-V-4/img/BinderExecuteAll.png)
+
 
 *)
 
--- tasks/german/installationsanweisungen.fsx
+++ tasks/german/installationsanweisungen.fsx
@@ -1,38 +1,83 @@
 (**
-// can't yet format YamlFrontmatter (["title: Installationsanweisungen"; "category: Übungen(deutsch)"; "categoryindex: 2"; "index: 1"], Some { StartLine = 2 StartColumn = 0 EndLine = 6 EndColumn = 8 }) to pynb markdown
+// can't yet format YamlFrontmatter (["title: Erste Schritte"; "category: Übungen(deutsch)"; "categoryindex: 2"; "index: 1"], Some { StartLine = 2 StartColumn = 0 EndLine = 6 EndColumn = 8 }) to pynb markdown
 
-# Installation
+# Erste Schritte
+
+## Installationsanweisung
 
 * Lade die neuste stabile Version von [Visual Studio Code](https://code.visualstudio.com/) herunter und installiere sie.
 * Lade die empfohlene [.NET SDK](https://dotnet.microsoft.com/download) herunter und installiere sie.
-* Oeffne Visual Studio Code, navigiere zum Reiter "Extensions" und installiere:
+* Öffne Visual Studio Code, navigiere zum Reiter "Extensions" und installiere:
     * .NET Interactive Notebooks
     * Ionide-fsharp
     
-    ![](https://raw.githubusercontent.com/CSBiology/BIO-BTE-12-V-4/main/docs/img/CodeExtensions.png)
+    ![](/BIO-BTE-12-V-4/img/CodeExtensions.png)
 
-* Lade das aktuelle Notizbuch von der links verlinkten Seite herunter.
 
-    ![](https://raw.githubusercontent.com/CSBiology/BIO-BTE-12-V-4/main/docs/img/DownloadNotebook.png)
+## Bearbeiten der Übungen
 
-* Druecke `Strg + Shift + P` oder `F1` in Visual Studio Code und klicke auf `.NET Interactive Open notebook`.
+Es gibt drei verschiedene Möglichkeiten zum bearbeiten der Übungen:
 
-    ![](https://raw.githubusercontent.com/CSBiology/BIO-BTE-12-V-4/main/docs/img/OpenNotebook.png)
+* Lokal als Notizbuch in Visual Studio Code
+* Lokal als fsx in Visual Studio Code
+* Online als Notizbuch auf Binder
 
-* Navigiere zum Speicherort deines Notizbuchs und oeffne es.
-* Notizbuecher beinhalten Text- und Codebloecke:
-    * Einen neuer Text- oder Codeblock kann hinzugefuegt werden, indem man mit dem Mauszeiger ueber den oberen oder unteren Rand eines vorhandenen Blocks faehrt:
+### Lokal als Notizbuch in Visual Studio Code
 
-    ![](https://raw.githubusercontent.com/CSBiology/BIO-BTE-12-V-4/main/docs/img/AddingBlock.png)
+* Lade das aktülle Notizbuch von der links verlinkten Seite herunter.
 
-    * Arbeiten mit Textbloecken:
-        Ein Textblock kann ueber einen Doppelklick bearbeitet werden. Innerhalb eines Textblocks kannst du normalen Text schreiben, oder den Text mit [Markdown](https://en.wikipedia.org/wiki/Markdown) anpassen.
+    ![](/BIO-BTE-12-V-4/img/DownloadNotebook.png)
+
+* Drücke `Strg + Shift + P` oder `F1` in Visual Studio Code und klicke auf `.NET Interactive Open notebook`.
+
+    ![](/BIO-BTE-12-V-4/img/OpenNotebook.png)
+
+* Navigiere zum Speicherort deines Notizbuchs und öffne es.
+* Notizbücher beinhalten Text- und Codeblöcke:
+    * Einen neür Text- oder Codeblock kann hinzugefügt werden, indem man mit dem Mauszeiger über den oberen oder unteren Rand eines vorhandenen Blocks fährt:
+
+    ![](/BIO-BTE-12-V-4/img/AddingBlock.png)
+
+    * Arbeiten mit Textblöcken:
+        Ein Textblock kann über einen Doppelklick bearbeitet werden. Innerhalb eines Textblocks kannst du normalen Text schreiben, oder den Text mit [Markdown](https://en.wikipedia.org/wiki/Markdown) anpassen.
         Sobald du fertig bist, kannst du die `Esc` Taste benutzen.
-    * Arbeiten mit Codebloecken:
-        Ein Codeblock kann ueber einen Klick in dne Block bearbeitet werden. In diesem Block kannst du deinen eigenen Code schreiben, oder bereits existierenden Code bearbeiten. Sobald du mit dem bearbeiten fertig bist, kannst du `Strg + Alt + Enter` druecken, um den Codeblock auszufuehren.
-        Solltest du alle Codebloecke auf einmal ausfuehren wollen, kannst du auf die beiden Pfeile in der oberen rechten Ecke des Notizbuches klicken:
+    * Arbeiten mit Codeblöcken:
+        Ein Codeblock kann über einen Klick in dne Block bearbeitet werden. In diesem Block kannst du deinen eigenen Code schreiben, oder bereits existierenden Code bearbeiten. Sobald du mit dem bearbeiten fertig bist, kannst du `Strg + Alt + Enter` drücken, um den Codeblock auszuführen.
+        Solltest du alle Codeblöcke auf einmal ausführen wollen, kannst du auf die beiden Pfeile in der oberen rechten Ecke des Notizbuches klicken:
 
-    ![](https://raw.githubusercontent.com/CSBiology/BIO-BTE-12-V-4/main/docs/img/ExecuteAll.png)
+    ![](/BIO-BTE-12-V-4/img/ExecuteAll.png)
+
+### Lokal als fsx in Visual Studio Code
+
+* Lade die aktülle fsx von der links verlinkten Seite herunter.
+
+    ![](/BIO-BTE-12-V-4/img/DownloadScript.png)
+
+* Drücke `Strg + O` in Visual Studio Code, navigiere zum Speicherort deiner fsx und öffne sie.
+* Du kannst, ähnlich wie bei einem Textfile, Code überall in der fsx schreiben. Über `Alt + Enter` kann der geschriebene Code ausgeführt werden
+    * Das drücken von `Alt + Enter` in einer Zeile führt die aktülle Zeile aus
+    * Wenn man einen Abschnitt markiert und dann `Alt + Enter` drückt, wird der markierte Abschnitt ausgeführt
+* Du kannst deinen Code kommentieren, indem du "//" vor deinen Kommentar setzt (z.B..: // Mein Kommentar)
+
+### Online als Notizbuch auf Binder
+
+* Öffne das aktülle Notizbuch auf der links verlinkten Seite.
+
+    ![](/BIO-BTE-12-V-4/img/RunBinder.png)
+
+* Notizbücher beinhalten Text- und Codeblöcke:
+    * Man kann einen neün Text- oder Codeblock über das klicken von `Insert` am oberen Ende der Seite und dem Auswählen der Eigenschaften `Code` oder `Markdown` erstellen:
+
+    ![](/BIO-BTE-12-V-4/img/BinderAddingBlock.png)
+
+    * Arbeiten mit Textblöcken:
+        Ein Textblock kann über einen Doppelklick bearbeitet werden. Innerhalb eines Textblocks kannst du normalen Text schreiben, oder den Text mit [Markdown](https://en.wikipedia.org/wiki/Markdown) anpassen.
+        Sobald du fertig bist, kannst du `Strg + Enter` drücken.
+    * Arbeiten mit Codeblöcken:
+        Ein Codeblock kann über einen Klick in dne Block bearbeitet werden. In diesem Block kannst du deinen eigenen Code schreiben, oder bereits existierenden Code bearbeiten. Sobald du mit dem bearbeiten fertig bist, kannst du `Strg + Enter` drücken, um den Codeblock auszuführen.
+        Solltest du alle Codeblöcke auf einmal ausführen wollen, kannst du auf die beiden Pfeile am oberen Ende des Notizbuches klicken:
+
+    ![](/BIO-BTE-12-V-4/img/BinderExecuteAll.png)
 
 *)
 
