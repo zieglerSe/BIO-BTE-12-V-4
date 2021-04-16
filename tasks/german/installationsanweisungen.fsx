6adfdf1ca8691e6bc39c69a221389486addcd61b
Joott <39764934+Joott@users.noreply.github.com>
2021-04-16 20:33:41 +0000

    Deploying to gh-pages from  @ 4a9ba930718a5c78248308295a5549a58f563690 🚀

--- tasks/german/installationsanweisungen.fsx
+++ tasks/german/installationsanweisungen.fsx
@@ -24,7 +24,7 @@ Es gibt drei verschiedene Möglichkeiten zum bearbeiten der Übungen:
 
 ### Lokal als Notizbuch in Visual Studio Code
 
-* Lade das aktülle Notizbuch von der links verlinkten Seite herunter.
+* Lade das aktuelle Notizbuch von der links verlinkten Seite herunter.
 
     ![](/BIO-BTE-12-V-4/img/DownloadNotebook.png)
 
@@ -34,7 +34,7 @@ Es gibt drei verschiedene Möglichkeiten zum bearbeiten der Übungen:
 
 * Navigiere zum Speicherort deines Notizbuchs und öffne es.
 * Notizbücher beinhalten Text- und Codeblöcke:
-    * Einen neür Text- oder Codeblock kann hinzugefügt werden, indem man mit dem Mauszeiger über den oberen oder unteren Rand eines vorhandenen Blocks fährt:
+    * Einen neuer Text- oder Codeblock kann hinzugefügt werden, indem man mit dem Mauszeiger über den oberen oder unteren Rand eines vorhandenen Blocks fährt:
 
     ![](/BIO-BTE-12-V-4/img/AddingBlock.png)
 
@@ -49,24 +49,24 @@ Es gibt drei verschiedene Möglichkeiten zum bearbeiten der Übungen:
 
 ### Lokal als fsx in Visual Studio Code
 
-* Lade die aktülle fsx von der links verlinkten Seite herunter.
+* Lade die aktuelle fsx von der links verlinkten Seite herunter.
 
     ![](/BIO-BTE-12-V-4/img/DownloadScript.png)
 
 * Drücke `Strg + O` in Visual Studio Code, navigiere zum Speicherort deiner fsx und öffne sie.
 * Du kannst, ähnlich wie bei einem Textfile, Code überall in der fsx schreiben. Über `Alt + Enter` kann der geschriebene Code ausgeführt werden
-    * Das drücken von `Alt + Enter` in einer Zeile führt die aktülle Zeile aus
+    * Das drücken von `Alt + Enter` in einer Zeile führt die aktuelle Zeile aus
     * Wenn man einen Abschnitt markiert und dann `Alt + Enter` drückt, wird der markierte Abschnitt ausgeführt
 * Du kannst deinen Code kommentieren, indem du "//" vor deinen Kommentar setzt (z.B..: // Mein Kommentar)
 
 ### Online als Notizbuch auf Binder
 
-* Öffne das aktülle Notizbuch auf der links verlinkten Seite.
+* Öffne das aktuelle Notizbuch auf der links verlinkten Seite.
 
     ![](/BIO-BTE-12-V-4/img/RunBinder.png)
 
 * Notizbücher beinhalten Text- und Codeblöcke:
-    * Man kann einen neün Text- oder Codeblock über das klicken von `Insert` am oberen Ende der Seite und dem Auswählen der Eigenschaften `Code` oder `Markdown` erstellen:
+    * Man kann einen neuen Text- oder Codeblock über das klicken von `Insert` am oberen Ende der Seite und dem Auswählen der Eigenschaften `Code` oder `Markdown` erstellen:
 
     ![](/BIO-BTE-12-V-4/img/BinderAddingBlock.png)
 
@@ -74,7 +74,7 @@ Es gibt drei verschiedene Möglichkeiten zum bearbeiten der Übungen:
         Ein Textblock kann über einen Doppelklick bearbeitet werden. Innerhalb eines Textblocks kannst du normalen Text schreiben, oder den Text mit [Markdown](https://en.wikipedia.org/wiki/Markdown) anpassen.
         Sobald du fertig bist, kannst du `Strg + Enter` drücken.
     * Arbeiten mit Codeblöcken:
-        Ein Codeblock kann über einen Klick in dne Block bearbeitet werden. In diesem Block kannst du deinen eigenen Code schreiben, oder bereits existierenden Code bearbeiten. Sobald du mit dem bearbeiten fertig bist, kannst du `Strg + Enter` drücken, um den Codeblock auszuführen.
+        Ein Codeblock kann über einen Klick in den Block bearbeitet werden. In diesem Block kannst du deinen eigenen Code schreiben, oder bereits existierenden Code bearbeiten. Sobald du mit dem bearbeiten fertig bist, kannst du `Strg + Enter` drücken, um den Codeblock auszuführen.
         Solltest du alle Codeblöcke auf einmal ausführen wollen, kannst du auf die beiden Pfeile am oberen Ende des Notizbuches klicken:
 
     ![](/BIO-BTE-12-V-4/img/BinderExecuteAll.png)
