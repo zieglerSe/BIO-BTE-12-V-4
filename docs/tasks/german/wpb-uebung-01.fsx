3d5226f8282151e89119da42e667b46b09d95ca5
Kevin Schneider <schneider.kev@outlook.de>
2021-04-16 14:30:19 +0200

    fix badge paths

--- docs/tasks/german/wpb-uebung-01.fsx
+++ docs/tasks/german/wpb-uebung-01.fsx
@@ -10,9 +10,9 @@ index: 2
 (**
 # Übung 1
 
-[![Binder](/img/badge-binder.svg)](https://mybinder.org/v2/gh/csbiology/BIO-BTE-12-V-4/gh-pages?filepath={{fsdocs-source-basename}}.ipynb)&emsp;
-[![Script](/img/badge-script.svg)]({{root}}{{fsdocs-source-basename}}.fsx)&emsp;
-[![Notebook](/img/badge-notebook.svg)]({{root}}{{fsdocs-source-basename}}.ipynb)
+[![Binder]({{root}}img/badge-binder.svg)](https://mybinder.org/v2/gh/csbiology/BIO-BTE-12-V-4/gh-pages?filepath={{fsdocs-source-basename}}.ipynb)&emsp;
+[![Script]({{root}}img/badge-script.svg)]({{root}}{{fsdocs-source-basename}}.fsx)&emsp;
+[![Notebook]({{root}}img/badge-notebook.svg)]({{root}}{{fsdocs-source-basename}}.ipynb)
 *)
 
 //************************************************************************************
