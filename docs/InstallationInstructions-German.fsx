15c0045fd7050c41cb6aea641af504b5ac631030
Jonathan Ott <jonathan.ott1994@gmail.com>
2021-04-16 11:31:18 +0200

    Add german installation instructions

--- docs/InstallationInstructions-German.fsx
+++ docs/InstallationInstructions-German.fsx
@@ -1,1 +1,34 @@
+(**
+# Installation
 
+* Lade die neuste stabile Version von [Visual Studio Code](https://code.visualstudio.com/) herunter und installiere sie.
+* Lade die empfohlene [.NET SDK](https://dotnet.microsoft.com/download) herunter und installiere sie.
+* Öffne Visual Studio Code, navigiere zum Reiter "Extensions" und installiere:
+    * .NET Interactive Notebooks
+    * Ionide-fsharp
+    
+    ![](https://raw.githubusercontent.com/CSBiology/BIO-BTE-12-V-4/main/docs/img/CodeExtensions.png)
+
+* Lade das aktuelle Notizbuch von der links verlinkten Seite herunter.
+
+    ![](https://raw.githubusercontent.com/CSBiology/BIO-BTE-12-V-4/main/docs/img/DownloadNotebook.png)
+
+* Drücke `Strg + Shift + P` oder `F1` in Visual Studio Code und klicke auf `.NET Interactive Open notebook`.
+
+    ![](https://raw.githubusercontent.com/CSBiology/BIO-BTE-12-V-4/main/docs/img/OpenNotebook.png)
+
+* Navigiere zum Speicherort deines Notizbuchs und öffne es.
+* Notizbücher beinhalten Text- und Codeblöcke:
+    * Einen neuer Text- oder Codeblock kann hinzugefügt werden, indem man mit dem Mauszeiger über den oberen oder unteren Rand eines vorhandenen Blocks fährt:
+
+    ![](https://raw.githubusercontent.com/CSBiology/BIO-BTE-12-V-4/main/docs/img/AddingBlock.png)
+
+    * Arbeiten mit Textblöcken:
+        Ein Textblock kann über einen Doppelklick bearbeitet werden. Innerhalb eines Textblocks kannst du normalen Text schreiben, oder den Text mit [Markdown](https://en.wikipedia.org/wiki/Markdown) anpassen.
+        Sobald du fertig bist, kannst du die `Esc` Taste benutzen.
+    * Arbeiten mit Codeblöcken:
+        Ein Codeblock kann über einen Klick in dne Block bearbeitet werden. In diesem Block kannst du deinen eigenen Code schreiben, oder bereits existierenden Code bearbeiten. Sobald du mit dem bearbeiten fertig bist, kannst du `Strg + Alt + Enter` drücken, um den Codeblock auszuführen.
+        Solltest du alle Codeblöcke auf einmal ausführen wollen, kannst du auf die beiden Pfeile in der oberen rechten Ecke des Notizbuches klicken:
+
+    ![](https://raw.githubusercontent.com/CSBiology/BIO-BTE-12-V-4/main/docs/img/ExecuteAll.png)
+*)
