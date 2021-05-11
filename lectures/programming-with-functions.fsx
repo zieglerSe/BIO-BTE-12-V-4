2e83e52ee8df5169b91b2c80452451eb97e3ebec
kMutagene <schneider.kev@outlook.de>
2021-05-11 11:03:02 +0000

    Deploying to gh-pages from  @ 55a80ecacf428e12cb97e523dc50f13ca44044a9 🚀

---
+++ lectures/programming-with-functions.fsx
@@ -0,0 +1,35 @@
+(**
+// can't yet format YamlFrontmatter (["title: WPB IV - Programming with functions"; "category: Lecture material"; "categoryindex: 1"; "index: 4"], Some { StartLine = 1 StartColumn = 0 EndLine = 5 EndColumn = 8 }) to pynb markdown
+
+# WPB IV - Programming with functions
+
+Towards a deeper understanding of functional programming:
+
+- WPB IV.01 : Currying und Closures / Currying and closures
+- WPB IV.02 : Funktionen h�herer Ordnung / Higher order functions
+- WPB IV.03 : Programmieren mit Funktionen / Programming with functions
+ 
+## Lecture videos (click to expand and watch)
+
+- <a href="#vimeo-1" data-action="collapse">WPB IV.01 </a></p>
+<div id="vimeo-1" class="is-collapsible">
+    <iframe title="vimeo-player" src="https://player.vimeo.com/video/417322489" width="640" height="360" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+</div>
+- <a href="#vimeo-2" data-action="collapse">WPB IV.02 </a></p>
+<div id="vimeo-2" class="is-collapsible">
+    <iframe title="vimeo-player" src="https://player.vimeo.com/video/417522824" width="640" height="360" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+</div>
+- <a href="#vimeo-3" data-action="collapse">WPB IV.03 </a></p>
+<div id="vimeo-3" class="is-collapsible">
+    <iframe title="vimeo-player" src="https://player.vimeo.com/video/417737643" width="640" height="360" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+</div>
+
+## Slides
+
+- WPB IV.01 ([deutsch](https://csb.bio.uni-kl.de/teaching/WPB_Podcast/IV01_Currying_und_Closures.pdf) / english)
+- WPB IV.02 ([deutsch](https://csb.bio.uni-kl.de/teaching/WPB_Podcast/IV02_Funktionen_hoeherer_Ordnung.pdf) / english)
+- WPB IV.03 ([deutsch](https://csb.bio.uni-kl.de/teaching/WPB_Podcast/IV03_Programmieren_mit_Funktionen.pdf) / english)
+
+
+
+*)
