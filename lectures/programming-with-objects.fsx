a955cd41312a71a73c4faff0792407a919fcd542
kMutagene <schneider.kev@outlook.de>
2021-05-18 13:01:13 +0000

    Deploying to gh-pages from  @ 6806640340eb0a401a7cf0201e9abbf8802ba9be 🚀

---
+++ lectures/programming-with-objects.fsx
@@ -0,0 +1,35 @@
+(**
+// can't yet format YamlFrontmatter (["title: WPB IV - Programming with objects"; "category: Lecture material"; "categoryindex: 1"; "index: 5"], Some { StartLine = 1 StartColumn = 0 EndLine = 5 EndColumn = 8 }) to pynb markdown
+
+# WPB IV - Programming with functions
+
+An introduction to programming with objects in FSharp
+
+- WPB V.01 : Veränderliche Werte / Mutable values
+- WPB V.02 : Vererbung / Inheritance
+- WPB V.03 : Polymorphismus / Polymorphism
+ 
+## Lecture videos (click to expand and watch)
+
+- <a href="#vimeo-1" data-action="collapse">WPB V.01 </a></p>
+<div id="vimeo-1" class="is-collapsible">
+    <iframe title="vimeo-player" src="https://player.vimeo.com/video/420275523" width="640" height="360" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+</div>
+- <a href="#vimeo-2" data-action="collapse">WPB V.02 </a></p>
+<div id="vimeo-2" class="is-collapsible">
+    <iframe title="vimeo-player" src="https://player.vimeo.com/video/420419644" width="640" height="360" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+</div>
+- <a href="#vimeo-3" data-action="collapse">WPB V.03 </a></p>
+<div id="vimeo-3" class="is-collapsible">
+    <iframe title="vimeo-player" src="https://player.vimeo.com/video/420639045" width="640" height="360" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+</div>
+
+## Slides
+
+- WPB V.01 ([deutsch](https://csb.bio.uni-kl.de/teaching/WPB_Podcast/V01_Veraenderliche_Werte.pdf) / [english](https://csb.bio.uni-kl.de/teaching/WPB_Podcast/eng/V01_Mutable_Values.pdf))
+- WPB V.02 ([deutsch](https://csb.bio.uni-kl.de/teaching/WPB_Podcast/V02_Vererbung.pdf) / [english](https://csb.bio.uni-kl.de/teaching/WPB_Podcast/eng/V02_Inheritance.pdf))
+- WPB V.03 ([deutsch](https://csb.bio.uni-kl.de/teaching/WPB_Podcast/V03_Polymorphismus.pdf) / [english](https://csb.bio.uni-kl.de/teaching/WPB_Podcast/eng/V03_Polymorphism.pdf))
+
+
+
+*)
