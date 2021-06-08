053ff3de1d752e25899c59ae09ff326dfae7180e
kMutagene <schneider.kev@outlook.de>
2021-06-08 12:47:04 +0000

    Deploying to gh-pages from  @ 6ca263b497f849935f5799b0df85a563bbbb278b 🚀

---
+++ lectures/descriptive-statistics-in-fsharp.fsx
@@ -0,0 +1,29 @@
+(**
+// can't yet format YamlFrontmatter (["title: WPB VIII - Descriptive statistics in FSharp"; "category: Lecture material"; "categoryindex: 1"; "index: 8"], Some { StartLine = 1 StartColumn = 0 EndLine = 5 EndColumn = 8 }) to pynb markdown
+
+# WPB VIII - Descriptive statistics in FSharp
+
+Using FSharp to perform basic statistics
+
+- WPB VIII.01 : Streuungsmaße mit FSharp.Stats / Measures of dispersion using FSharp.Stats
+- WPB VIII.02 : T-Test mit FSharp.Stats / T test using FSharp.Stats
+
+ 
+
+## Lecture videos (click to expand and watch)
+
+- <a href="#vimeo-1" data-action="collapse">WPB VIII.01 </a></p>
+<div id="vimeo-1" class="is-collapsible">
+    <iframe title="vimeo-player" src="https://player.vimeo.com/video/428055936" width="640" height="360" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+</div>
+- <a href="#vimeo-2" data-action="collapse">WPB VIII.02 </a></p>
+<div id="vimeo-2" class="is-collapsible">
+    <iframe title="vimeo-player" src="https://player.vimeo.com/video/428405483" width="640" height="360" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+</div>
+
+## Slides
+
+- WPB VIII.01 ([deutsch](https://csb.bio.uni-kl.de/teaching/WPB_Podcast/VIII01_Streuungsma%C3%9Fe_mit_FSharpStats.pdf) /  [english](https://csb.bio.uni-kl.de/teaching/WPB_Podcast/eng/VIII01_Measures_of_dispersion_in_FSharpStats.pdf))
+- WPB VIII.02 ([deutsch](https://csb.bio.uni-kl.de/teaching/WPB_Podcast/VIII02_T-Test_mit_FSharpStats.pdf) / [english](https://csb.bio.uni-kl.de/teaching/WPB_Podcast/eng/VIII02_T-test_using_FSharpStats.pdf))
+
+*)
