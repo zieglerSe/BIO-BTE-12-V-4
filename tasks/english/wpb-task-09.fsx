0dc214d627d2e8b9e4429b9db668a8c1588c3db1
kMutagene <schneider.kev@outlook.de>
2021-06-15 12:28:07 +0000

    Deploying to gh-pages from  @ e71b169bddbc9add4ff621da683964acfd41b694 🚀

--- tasks/english/wpb-task-09.fsx
+++ tasks/english/wpb-task-09.fsx
@@ -5,128 +5,222 @@
 [![Script](/BIO-BTE-12-V-4/img/badge-script.svg)](/BIO-BTE-12-V-4/tasks/english/wpb-task-09.fsx)&emsp;
 [![Notebook](/BIO-BTE-12-V-4/img/badge-notebook.svg)](/BIO-BTE-12-V-4/tasks/english/wpb-task-09.ipynb)
 
-# Task09 - Data access
+# Task09 - Embracing bioinformatics in F#
 
-## 0 Preface
+## Preface
 
-The following documentation may be helpful in completing the tasks:
+You can find the Plotly.NET documentation here: https://plotly.net
 
-* Deedle: https://fslab.org/Deedle
+You can find the BioFSharp documentation here: https://csbiology.github.io/BioFSharp/
 
-### Referencing Plotly.NET, Deedle and FSharp.Stats
+## Referencing Plotly.NET and BioFSharp
 
-These lines always have to be executed once, otherwise you can not use the library:
-
-*)
-#r "nuget: Deedle, 2.3.0"
-open Deedle
-(**
-### Working with Deedle
-
-Should you see this error message:
-```
-9_Data_exploration_using_FSharp.fsx(113,5): error FS0030: Value restriction. The value 'cpw'' has been inferred to have generic type val cpw' : Series<(string * int),'_a>      
-Either define 'cpw'' as a simple data term, make it a function with explicit arguments or, if you do not intend for it to be generic, add a type annotation.
-```
-Then you should resort to an explicit type annotation.
-Instead of:
-```
-let cpw' = persons |> Frame.getCol "cpw"
-```
-You should use:
-```
-let cpw' :Series<int,float> = persons |> Frame.getCol "cpw"
-```
-To display deedle objects in this notebook you can use the `.Print()` member:
+These lines must always be executed at least once, otherwise Plotly.NET and BioFSharp cannot be used:
 
 *)
-let firstNames' = Series.ofValues ["Kevin";"Lukas";"Benedikt";"Michael"]
-firstNames'.Print()
-(**
-## 1 Basics
+#r "nuget: Plotly.NET, 2.0.0-preview.1"
+#r "nuget: Plotly.NET.Interactive, 2.0.0-preview.1"
+#r "nuget: BioFSharp, 1.2.0"
+#r "nuget: BioFSharp.IO, 1.2.0"
 
+// Plotly.NET and BioFSharp are opened here.
+open Plotly.NET
+open BioFSharp
+open BioFSharp.IO
+// Additional namespaces/modules are also opened here to simplify the signatures.
+open BioArray
+open BioSeq
+open AminoAcids
+open Nucleotides
+(**
+## Copying paths
+
+Reading in data will be discussed in more detail in a later exercise. In this exercise a file must be referenced and read in via a path.
+How to get the absolute path of a file is shown here.
+
+### Windows
+
+* Navigate to the downloaded file
+
+* Press `Shift` + `Right click` on the file
+
+* Select "Copy as path" from the context menu:
+
+    ![](/BIO-BTE-12-V-4/img/PfadWindows.png)
+
+* With `Ctrl` + `V` you can paste the copied path at any position
+
+### Mac
+
+* Navigate to the downloaded file
+
+* Press with `ctrl-click` or `right-click` on the file
+
+* With the context menu open, also hold down `Alt`
+
+* Select "Copy X as pathname" from the context menu:
+
+    ![](/BIO-BTE-12-V-4/img/PfadMac.png)
+
+* With `Command` + `V` you can paste the copied path at any position
+
+## Task 1
+
+### Task 1.0
+
+`nucleotideString` is a sequence of nucleotides. This sequence is to be translated into the corresponding amino acid sequence.
+
+* A Convert 'nucleotideString' to a BioArray.
+* B Transcribe the result from A (the given sequence is the template strand)
+* C Translate the result from B with an offset of 0
+* D Create a function that gets a string, executes the previous steps and thus returns a `BioArray<AminoAcids>`.
+
+*)
+let nucleotideString = "TACCATGCAGCTACCTAAGGATCACCGACT"
+(**
 ### Task 1.1
 
-Given are 4 series of equal length. Use the function `Series.mapValues` to double the values of "coffeesPerWeek".
+Download the nucleotide fasta of yeast (S. cerevisiae): https://github.com/CSBiology/BIO-BTE-12-V-4/releases/download/YeastFasta/orf_genomic_filtered.fasta. <br>
+Source: http://sgd-archive.yeastgenome.org/sequence/S288C_reference/orf_dna/ <br>
+Copy the path of the downloaded file and bind it to the name `filepath`.
+
+Read the fasta and bind the result to `yeastFasta`. Use the function 
+`FastA.fromFile` and use `BioArray.ofNucleotideString` as converter function.
+Convert the resulting sequence into an array.
 
 *)
-let firstNames      = Series.ofValues ["Kevin";"Lukas";"Benedikt";"Michael"] 
-let coffeesPerWeek  = Series.ofValues [15;12;10;11] 
-let lastNames       = Series.ofValues ["Schneider";"Weil";"Venn";"Schroda"]  
-let group           = Series.ofValues ["CSB";"CSB";"CSB";"MBS"]
+let filepath = "Your/Path/here"
 (**
 ### Task 1.2
 
-Create a frame named "persons" based on the 4 given Series.
+From the created sequence (yeastFasta), generate an array of headers and an array of
+sequences and bind these arrays to the names `header` and `dna`. 
+Hint: `yeastFasta` consists of FastaItems, which are record types consisting of `header` and `sequence`. 
+Since the whole exercise is based on these arrays, please report if it does not work.
 
 *)
 
 (**
 ### Task 1.3
 
-Add a column named "sodasPerWeek" to the frame. Bind the resulting frame to a name.
+Transcribe all nucleotide sequences from `dna` and bind the resulting array to the name 
+`rna`.
 
-Tip: Create a `Series<int,int>` first. Use `Frame.addCol`
+* Tip: The DNA sequences represent the coding strand.
+* Tip2: Use `Array.map` to iterate over the sequences (also applies to the following tasks).
 
 *)
 
 (**
 ### Task 1.4
 
-Add the columns "sodasPerWeek" and "coffeesPerWeek". Add the resulting series as a column with the title "allPurchases" to the previously created frame.
-
-Tip 1: This task can be solved in several ways.
-
-Tip 2: Via `Series.values` you can access the values of each Series. Then you could iterate over both collections with `Seq.map2`. 
+In the following, translate the RNA sequences into proteins, that is, amino acid sequences.   
+Bind the result to the name `proteins`.
 
 *)
 
 (**
 ### Task 1.5
 
-Determine the sum of "allPurchases".
+Now the proteins are to be digested tryptically to peptides (trypsin is a protease which cuts proteins 
+into lysine (single letter code K) and arginine (single letter code R).
+The function 'Digestion.BioArray.digest' expects a proteinID. Here any integer 
+can be used. Ideally, the index of the respective amino acid sequence should be entered here.
+Bind the result to 'digestedProteins'.
 
 *)
-
+let trypsin = Digestion.Table.Trypsin
 (**
-## 2 Frame Operationen
+### Task 1.6
 
+No task<br>
+The section below creates FastA items from the individual peptides and writes them to a 
+FastA file.
+
+*)
+let convertDigPepToFasta (digPep: Digestion.DigestedPeptide<'a>[])=
+    digPep
+    |> Array.mapi (fun index peptide ->
+        FastA.createFastaItem (sprintf "Protein %i Fragment %i" peptide.ProteinID index) peptide.PepSequence 
+        )
+
+// __SOURCE_DIRECTORY__ defines the path to the folder where this script is located.   
+digestedProteins
+|> Array.map convertDigPepToFasta
+|> Array.concat
+|> FastA.write BioItem.symbol (__SOURCE_DIRECTORY__ + "/digestedProteins.fasta")
+(**
 ### Task 2.1
 
-Group the rows of the frame from Task 1.2 according to the elements of the "group" column.
-
-Tip: Explicit type annotation (see: [Working with Deedle](#Working-with-Deedle)). 
+Below you can see the file path to the created peptide fasta.
+Read in the fasta and bind the result to 'peptides'. Use a suitable 
+Converter function
 
 *)
-
+let pathToPeptides = __SOURCE_DIRECTORY__ + "/digestedProteins.fasta"
 (**
-### Task 2.2
+### Task 2.2 (optional)
 
-Results tables often contain more than 40 columns. However, only a few are interesting for individual analysis. 
-It is therefore often useful to create a frame that contains fewer columns. Use the function `Frame.sliceCols` to create a frame on the base of the 
-frame from Task 1.2 to create a frame that contains only the columns "lastNames" and "coffeesPerWeek".
+Visualize the frequencies of the amino acids in a suitable diagram.
+e.g. histogram, column chart, pie chart or others.
+To convert `AminoAcids.AminoAcid` into chars (i.e. single letter codes) you can use `AminoAcids.symbol`.
 
 *)
 
 (**
 ### Task 2.3
 
-Many times you want to aggregate based on groupings. Calculate the sum of the "coffeePerWeek" column for each group.
-
-Tip: Extract the "coffeePerWeek" column from the result of Task 2.1. Proceed as demonstrated in the lecture. 
-
-*)
-
-(**
-### Task 2.4
-
-Often you want to save intermediate results. Save the frame from task 1.2 as a CSV file. Use ';' as separator.
+Create a histogram of the molecular weights of all proteins in `proteins`.
+Examine the `BioArray` module for a suitable function. 
+Label the axes.  
 
 *)
 
 (**
-### Task 2.5
+### Task 2.4 (optional)
 
-Use the function 'Frame.ReadCsv' to read the file again.
+There are also mitochondrial ORFs in the nucleotide fasta (Task 1.2). 
+Divide `yeastFasta` into two arrays. In one should be all nucleotide sequences from 'Chr I' to 
+Chr 'XVI', and in the other all from 'Chr Mito'. Transcribe the sequences and visualize the codon usage with the function defined below (possibly Chart.Stack 1?).
+
+* Highest percentage mitochondrion:           
+    * associated codon:
+    * associated amino acid:
+* Highest percentage nucleus:
+    * associated codon:
+    * associated amino acids:
 
 *)
+open Plotly.NET.Axis
+open Plotly.NET.StyleParam
+
+let myAxis title = LinearAxis.init(Title=title,Mirror=Mirror.All,Ticks=TickOptions.Inside,Showgrid=false,Showline=true,Zeroline=false)
+let styleChart x y chart = chart |> Chart.withX_Axis (myAxis x) |> Chart.withY_Axis (myAxis y)
+let plotCodonFrequency (rnaInput:BioArray.BioArray<Nucleotides.Nucleotide>[]) =
+    let allTripletts =
+        rnaInput
+        |> Array.map (fun singleRna -> Seq.chunkBySize 3 singleRna)
+        |> Seq.concat
+        |> Array.ofSeq
+    allTripletts
+    |> Seq.groupBy id
+    |> Seq.mapi (fun i (codon,items) -> 
+        printfn "step %i, Codon %A" i codon
+        let aminoAcid = Nucleotides.CodonMap.[codon.[0],codon.[1],codon.[2]]
+        let codonString = sprintf "%A%A%A" codon.[0] codon.[1] codon.[2]
+        aminoAcid,codonString,float (Seq.length items) / float (Seq.length allTripletts))
+    |> Seq.sortBy (fun (_,co,_) -> co)
+    |> Seq.groupBy (fun (aa,_,_) -> aa)
+    |> Seq.map (fun (aa,codons) -> 
+        codons
+        |> Seq.sortByDescending (fun (aa,codon,count) -> count)
+        |> Seq.map (fun (aa,codon,count) -> codon,count)
+        |> Chart.Column
+        |> Chart.withTraceName (sprintf "%A" aa)
+        )
+    |> Chart.Combine
+    |> styleChart "" "rel. count (%)"
+    |> Chart.withSize (900.,600.)
+    |> fun x -> 
+        Chart.Show x
+        x
